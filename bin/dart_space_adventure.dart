d07e90bf431f26f1e49ad8eeb8db5539dbeeb2a5
clay-town <justinmccune@icloud.com>
2020-10-17 19:43:45 -0700

    Add user input for name

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,11 +1,13 @@
-import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+//import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+import 'dart:io';
 
 void main(List<String> arguments) {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
   print('What is your name?');
-  print('Jane');
-  print('Nice to meet you, Jane. My name is Eliza, I\'m an old friend of Alexa.');
+  String name = stdin.readLineSync();
+  
+  print('Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
   print('Let\'s go on an adventure!');
   print('Shall I randomly choose a planet for your to visit? (Y or N)');
   print('N');
